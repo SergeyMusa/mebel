4ec4a29251dd21a9d3ff1611c0fa32c1eb40b8e1
SergeyMusa <sir.stv@gmail.com>
2022-06-21 09:27:56 +0300

    first step

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>БОБЕР КРАСАВА!</div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderComponent from "@/components/Header";
+
+export default {
+  name: "HomeView",
+  components: {
+    // HelloWorld,
+    HeaderComponent,
+  },
+};
+</script>
